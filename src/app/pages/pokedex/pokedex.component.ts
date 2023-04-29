b499c70d1a1d2f9f4d324b559c6855edbc43f7f9
Vanderson-Oliveira12 <vanderson.doliveira14@gmail.com>
2023-04-29 15:07:20 -0300

    adjusting type colors and increment filter checkbox

--- src/app/pages/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/pokedex.component.ts
@@ -116,7 +116,52 @@ export class PokedexComponent implements OnInit {
         return typeIng = "Gelo";
       } else if(typePokemon == "ghost"){
         return typeIng = "Fantasma";
+      } else if(typePokemon == "dragon"){
+        return typeIng = "Dragão";
       }
+      return "";
+  }
+
+  convertColorBackground(color: string): string{
+    let background_color = ""
+
+    if(color == "grass"){
+      return background_color = "#048002";
+   } else if(color == "poison"){
+     return background_color = "#7f037f";
+   } else if(color == "fire"){
+     return background_color = "#f23219";
+   } else if(color == "water"){
+     return background_color = "#00a8ff";
+   } else if(color == "bug"){
+     return background_color = "#427a35";
+   } else if(color == "flying"){
+     return background_color = "#dac4be";
+   } else if(color == "normal"){
+     return background_color = "#e6a69a";
+   } else if(color == "electric"){
+     return background_color = "#b5bd00";
+   } else if(color == "ground"){
+     return background_color = "#8e413a";
+   } else if(color == "fairy"){
+     return background_color = "#ffabc6";
+   } else if(color == "fighting"){
+     return background_color = "#e68378";
+   } else if(color == "psychic"){
+     return background_color = "#d5db00";
+   } else if(color == "rock"){
+     return background_color = "#202020";
+   } else if(color == "steel"){
+     return background_color = "#808080";
+   } else if(color == "ice"){
+     return background_color = "#d8f1f5";
+   } else if(color == "ghost"){
+     return background_color = "#311ab8";
+   } else if(color == "dragon"){
+    return background_color = "#beda86";
+  }
+   return "";
+
       return "";
   }
 
